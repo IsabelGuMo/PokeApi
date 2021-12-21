ec151a07c812cc44cdd7abf8291515a69ba4afd9
Isabel Gutiérrez <gm.isabel@gmail.com>
2021-12-21 21:15:55 +0100

    correcciones y estilos CSS

--- pokedex.js
+++ pokedex.js
@@ -10,17 +10,17 @@ const getPokemonsApi = async () => {
 }
 
 const printPokemon = (pokemon) => {
+    const newPokemon = document.createElement("div");
+    newPokemon.classList.add("col-4");
+    
     const pokemonHTML =`
         <div class="card pokemon-card">
-            <img src="${pokemon.sprites.front_default}" width="96" class="mx-auto d-block" "rounded-pill"/>
+            <img src="${pokemon.sprites.front_default}" width="96" class="mx-auto d-block"/>
             <div class="card-body text-center" </div>
                 <h5 class="card-title">${pokemon.name}</h5>
             </div>
         </div>
     `;
-    const newPokemon = document.createElement("div");
-    newPokemon.classList.add("col-4");
-    
     newPokemon.innerHTML = pokemonHTML;
 
     pokemonsContainer$$.appendChild(newPokemon);
